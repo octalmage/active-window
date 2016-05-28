1f71ab02271b0a69fb2e19fa5adfc7c697faeb65
Robson Vieira <robsonvnasc@gmail.com>
2016-05-28 13:01:34 -0300

    Added support to Windows 7+ and Linux (#1)
    * Added: support to windows, linux and mac; option to track active window..
    
    * Updated readme TODO
    
    * Update README.md
    
    Fixed TODO list;
    
    * Removed usage.js trash
    
    * Fixed file directory bug on plugin
    
    * Now treating linux apps response
    
    * Fixing windows compatibility
    
    * Mergin version
    
    * fixing merge
    
    * Fixing windows get bug
    
    * Fixed Windows Bug getActiveWindow
    
    * Unified getActiveWindow and watchActiveWindow; Fully working on Windows 10
    
    * Fixing infinity decrease
    
    * Fixed linux if condition
    
    * Update README.md
    
    Update tested OS list
    
    * Update README.md
    
    Added javascript template
    
    * Tested on Debian 8
    
    * Update README.md
    
    * Added support to Mac OS
    
    * Tested on OSX Yosemite
    
    * Fixed Windows BUG
    
    * Fixed Windows Bug
    
    * Fixed OSX bug
    
    * Fixed Linux 0 folder bug
    
    * Fixed OSX bug
    
    * Tested on Windows 7

--- index.js
+++ index.js
@@ -1,41 +1,104 @@
-var exec = require("child_process").exec;
-var multiline = require("multiline");
+var fs = require('fs');
+var config = getConfig();
 
-//Applescript to get active window title.
-var getWindowScript = multiline(function(){/*
-global frontApp, frontAppName, windowTitle
+/**
+ * This callback handle the response request by getActiveWindow function
+ * @callback getActiveWindowCallback
+ * @param {app: string, window: string} window
+ */
 
-set windowTitle to ""
-tell application "System Events"
-	set frontApp to first application process whose frontmost is true
-	set frontAppName to name of frontApp
-	tell process frontAppName
-		tell (1st window whose value of attribute "AXMain" is true)
-			set windowTitle to value of attribute "AXTitle"
-		end tell
-	end tell
-end tell
+/**
+* Get the active window
+* @param {getActiveWindowCallback} callback - The callback that handles the response.
+* @param {integer} [repeats  = 1] - Number of repeats; Use -1 to infinity repeats
+* @param {float}   [interval = 0] - Loop interval in seconds. For milliseconds use fraction (0.1 = 100ms)
+*/
+exports.getActiveWindow = function(callback,repeats,interval){
+  const spawn = require('child_process').spawn;
 
-return {frontAppName, windowTitle}
-*/});
+  interval = (interval) ?  interval : 0;
+  repeats = (repeats) ? repeats : 1;
 
-module.exports = function(callback)
-{
-    //Run applescript from shell.
-    var script = "osascript -e '" + getWindowScript + "'";
-        
-    exec(script, function(error, stdout, stderr)
-    {
-        var active = {};
-        var active_a = stdout.split(",");
-        
-        if (typeof active_a[0] !== "undefined")
-            active.app = active_a[0];
-    
-        
-        if (typeof active_a[1] !== "undefined")
-            active.title = active_a[1].replace(/\n$/, "").replace(/^\s/, "");
-    
-        callback(active);
-    });
-};
+  //Scape negative number of repeats on Windows OS
+  if (process.platform == 'win32' && repeats < 0 ){
+    repeats = '\\-1';
+  }
+
+  parameters  = config.parameters;
+  parameters.push(repeats);
+  parameters.push(interval);
+
+  //Run shell script
+  const ls  = spawn(config.bin,parameters);
+  ls.stdout.setEncoding('utf8');
+
+  //Obtain successful response from script
+  ls.stdout.on('data', function(stdout){
+    callback(reponseTreatment(stdout.toString()));
+  });
+
+  //Obtain error response from script
+  ls.stderr.on("data",function(stderr){
+   throw stderr.toString();
+  });
+
+  ls.stdin.end();
+}
+
+/**
+* Treat and format the response string and put it into a object
+* @function reponseTreatment
+* @param {string} String received from script
+*/
+function reponseTreatment(response){
+  window = {};
+  if(process.platform == 'linux'){
+    response = response.replace(/(WM_CLASS|WM_NAME)(\(\w+\)\s=\s)/g,'').split("\n",2);
+    window.app = response[0];
+    window.title = response[1];
+  }else if (process.platform == 'win32'){
+    response = response.replace(/(@{ProcessName=| AppTitle=)/g,'').slice(0,-1).split(';',2);
+    window.app = response[0];
+    window.title = response[1];
+  }else if(process.platform == 'darwin'){
+    response = response.split(",");
+    window.app = response[0];
+    window.title = response[1].replace(/\n$/, "").replace(/^\s/, "");
+  }
+  return window;
+}
+
+/**
+* Get script config accordingly the operating system
+* @function getConfig
+*/
+function getConfig(){
+  //Retrieve configs
+  var configs = JSON.parse(fs.readFileSync(__dirname+'/configs.json', 'utf8'));
+  var path = require("path");
+
+  switch(process.platform){
+      case 'linux':
+      case 'linux2':
+          config = configs.linux
+          break;
+      case 'win32':
+          config = configs.win32
+          break;
+      case 'darwin':
+          config = configs.mac;
+          break;
+      default:
+          throw "Operating System not supported yet. "+process.platform;
+  }
+  //Append directory to script url
+  script_url = path.join(__dirname,config.script_url);
+  config.parameters.push(script_url);
+
+  //Append directory to subscript url on OSX
+  if(process.platform=="darwin"){
+    config.parameters.push(path.join(__dirname,config.subscript_url));
+  }
+
+  return config;
+}
